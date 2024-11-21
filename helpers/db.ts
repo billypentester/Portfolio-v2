b4a8d179c289cd621bb2668ac75385e5f4eeab95
Bilal Ahmad <bilal.ahmad@simplexts.biz>
2024-11-21 22:36:43 +0500

    query change

--- helpers/db.ts
+++ helpers/db.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@/config/db";
 export async function fetchTheme() {
 
     try {
-        const response = await useQuery("SELECT name FROM theme limit ?", [1]);
+        const response = await useQuery("SELECT name FROM theme limit 1", []);
         const data: any = (response);
         return data[0].name;
     }
