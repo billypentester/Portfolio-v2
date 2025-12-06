53d33a51fe102febdb4d0c166704984deed211ef
Bilal Ahmad <bilalsheikh2500@gmail.com>
2025-12-06 15:43:30 +0500

    reolve issue

--- src/helpers/IconBuilder.tsx
+++ src/helpers/IconBuilder.tsx
@@ -1,7 +1,7 @@
 import { icons } from "@/src/config/Icon"
 import { IconBuilderInterface } from "../lib/interface"
 
-export default function IconBuilder({ type, paint }: IconBuilderInterface) : JSX.Element {
+export default function IconBuilder({ type, paint }: IconBuilderInterface) {
     return (
         <svg 
             xmlns="http://www.w3.org/2000/svg" 
